899c6a89a7398c8bec9a23a6da3059ebd7927340
McloiGG <marcusloiminghan@gmail.com>
2026-08-15 16:46:56 +0800

    green: removing an item drops the count and has() stops finding it

--- ex00/Inventory.cpp
+++ ex00/Inventory.cpp
@@ -7,7 +7,14 @@ void	Inventory::add(const std::string& item)
 
 void	Inventory::remove(const std::string& item)
 {
-	(void) item;
+	for (auto it = m_items.begin(); it != m_items.end(); ++it)
+	{
+		if (*it == item)
+		{
+			m_items.erase(it);
+			return;
+		}
+	}
 }
 
 bool	Inventory::has(const std::string& item) const
