c1ac8d3f268b0f6565a83d124881f7ff311e679d
McloiGG <marcusloiminghan@gmail.com>
2026-08-15 19:48:01 +0800

    feat: completed ex02

--- ex02/Dice.hpp
+++ ex02/Dice.hpp
@@ -2,16 +2,16 @@
 #ifndef DICE_HPP
 # define DICE_HPP
 
-# include <cstdlib>
-
-struct RandomSource;
+# include "RandomSource.hpp"
+// # include <cstdlib>
 
 class Dice
 {
 public:
 	explicit Dice(RandomSource& rng) : m_rng(rng) {}
 
-	int	roll(){ return std::rand() % 6 + 1; }
+	int	roll(){ return m_rng.next(6) + 1; }
+	// int	roll(){ return std::rand() % 6 + 1; }
 
 private:
 	RandomSource&	m_rng;
